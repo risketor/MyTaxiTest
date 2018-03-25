44d12c347cc2845afc3cbd95dbc35a63b3f1d44d
risketor <37552903+risketor@users.noreply.github.com>
2018-03-25 11:15:40 +0100

    Create TestData.java

---
+++ TestData.java
@@ -0,0 +1,21 @@
+package com.mytaxi.android_demo;
+
+/**
+ * Created by AL on 23/03/2018.
+ * Test Data object file, with data for testing.
+ */
+
+public class TestData {
+
+    // Valid user details
+    public class User{
+        public static final String VALID_USERNAME = "whiteelephant261";
+        public static final String VALID_PASSWORD = "video";
+    }
+
+    // Search details
+    public class Search{
+        public static final String VALID_SEARCH_QUERY = "sa";
+        public static final String VALID_SEARCH_NAME_TO_SELECT = "Sarah Friedrich";
+    }
+}
